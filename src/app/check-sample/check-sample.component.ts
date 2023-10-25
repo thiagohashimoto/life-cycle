88c4666ece5bd842fad326780d6560e57b66c685
PC Thiago <thiago.hashimoto94@gmail.com>
2023-10-24 22:07:43 -0300

    Trabalhando com ng components do Angular

---
+++ src/app/check-sample/check-sample.component.ts
@@ -0,0 +1,61 @@
+import { 
+  Component, 
+  OnInit , 
+  DoCheck, 
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  OnDestroy
+} from '@angular/core';
+
+@Component({
+  selector: 'app-check-sample',
+  templateUrl: './check-sample.component.html',
+  styleUrls: ['./check-sample.component.css']
+})
+export class CheckSampleComponent implements 
+OnInit, 
+DoCheck, 
+AfterContentChecked,
+AfterContentInit,
+AfterViewChecked,
+AfterViewInit,
+OnDestroy
+{
+
+  quantidade:number = 0
+
+  constructor(){ }
+  
+
+  adicionar(){
+    this.quantidade += 1
+  }
+  decrementar(){
+    this.quantidade -= 1
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck')
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngOnInit(): void {
+      console.log('ngOnInit')
+  }
+  ngOnDestroy(): void {
+    console.log('Goodbye My Friend')
+  }
+
+}
